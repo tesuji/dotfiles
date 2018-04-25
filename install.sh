f29badf3f38de926070ed92c9c0950a4aac908d0
lzutao <1stpangu@gmail.com>
2018-04-25 19:35:59 +0700

    few changes: * add userChrome.css to disable menu Firefox * format case structure in install.sh * make vmware mount functions more useful

--- install.sh
+++ install.sh
@@ -24,23 +24,24 @@ solve_stow_conflict() { # solve_stow_conflict path backup
 
   stow -nv -t "$HOME" "$path" 2>&1 | sed -n 's/^  \*.*: //p' | while read error; do
     error="${HOME}/${error}"
-    if [[ -d "$error" ]]; then
+    if [[ ( -f "$error" ) || ( -L "$error" ) ]]; then
+      case "$backup" in
+        (true)
+          echo "[!] Backup $1 to ${BACKUP_PATH}" 2>&1
+          mv "$error" -t "${BACKUP_PATH}"
+          ;;
+        (false)
+          rm -f "$error"
+          ;;
+        (*)
+          echo "[!] Error: Unknown option: ${backup}" 2>&1
+          exit 11
+          ;;
+      esac
+    else
       echo "\"$error\" is a folder. Exitting"
       exit 127
     fi
-    case "$backup" in
-      (true)
-        echo "[!] Backup $1 to ${BACKUP_PATH}" 2>&1
-        mv "$error" -t "${BACKUP_PATH}"
-        ;;
-      (false)
-        rm -f "$error"
-        ;;
-      (*)
-        echo "[!] Error: Unknown option: ${backup}" 2>&1
-        exit 11
-        ;;
-    esac
   done
 
   stow -t "$HOME" "$path"
