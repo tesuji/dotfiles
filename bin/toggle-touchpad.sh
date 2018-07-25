1fe7ff6fe6c9225aec80644bc07e662224804aed
Lzu Tao <1stpangu@gmail.com>
2018-07-26 00:22:34 +0700

    touchpad: Clean up toggle_touchpad function
    Remove function keyword in Bash, that make it looks like a C routine.
    Also, beginning word should be in Title Case.
    Additionally, insert space before and after pipe |.

--- bin/toggle-touchpad.sh
+++ bin/toggle-touchpad.sh
@@ -4,22 +4,29 @@
 ## Use at your own risk!
 #######################################################
 
-## more in https://askubuntu.com/questions/844151/enable-disable-touchpad/844218
-## or https://askubuntu.com/a/537031/565006
-function toggle_touchpad() {
-  ## fgrep and cut is still faster than sed
-  local ID=$( xinput | fgrep TouchPad |cut -f2 |cut -d= -f2 )
-  local STATE=$( xinput list-props "$ID" | grep "Device Enabled" | grep -o "[01]$" )
+## Ref:
+##    https://askubuntu.com/questions/844151/enable-disable-touchpad/844218
+##    https://askubuntu.com/a/537031/565006
+##    https://wiki.archlinux.org/index.php/Libinput#Disable_touchpad
+toggle_touchpad() {
   local icon_enable=input-touchpad-symbolic
   local icon_disable=touchpad-disabled-symbolic
+
+  local info_enable="Touchpad has been enabled"
+  local info_disable="Touchpad has been disabled"
+
+  ## fgrep and cut is still faster than sed
+  local ID=$( xinput | fgrep TouchPad | cut -f2 | cut -d= -f2 )
+  local STATE=$( xinput list-props "$ID" | grep "Device Enabled" | grep -o "[01]$" )
+
   if [[ "$STATE" -eq '1' ]]; then
     ## Should install Notification servers
     ## https://wiki.archlinux.org/index.php/Desktop_notifications#Notification_servers
     xinput --disable "$ID" && \
-        notify-send "Disabled" "Touchpad has been disabled" --icon="$icon_disable"
+        notify-send "Disabled" "$info_disable" --icon="$icon_disable"
   else
     xinput --enable "$ID" && \
-        notify-send "Enabled " "Touchpad has been enabled" --icon="$icon_enable"
+        notify-send "Enabled " "$info_enable" --icon="$icon_enable"
   fi
 }
 
