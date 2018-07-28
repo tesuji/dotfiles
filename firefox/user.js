693cb2e26322e36e80f45b727137f4cb2738d47b
Lzu Tao <1stpangu@gmail.com>
2018-07-29 00:49:53 +0700

    [firefox] Add new user.js to customize Firefox
    This user.js file helps us synchronize settings across Firefox
    version and multiple devices.

---
+++ firefox/user.js
@@ -0,0 +1,322 @@
+// Mozilla User Preferences
+
+/******************************************************************************
+ * user.js                                                                    *
+ * Inherit from https://github.com/pyllyukko/user.js                          *
+ ******************************************************************************/
+
+/******************************************************************************
+ * SECTION: HTML5 / APIs / DOM                                                *
+ ******************************************************************************/
+
+// PREF: Disable web notifications
+// https://support.mozilla.org/en-US/questions/1140439
+user_pref("dom.webnotifications.enabled",                       false);
+
+// PREF: Disable DOM timing API
+// https://wiki.mozilla.org/Security/Reviews/Firefox/NavigationTimingAPI
+// https://www.w3.org/TR/navigation-timing/#privacy
+user_pref("dom.enable_performance",                             false);
+
+// PREF: Disable Location-Aware Browsing (geolocation)
+// https://www.mozilla.org/en-US/firefox/geolocation/
+user_pref("geo.enabled",                                        false);
+
+// PREF: When geolocation is enabled, use Mozilla geolocation service instead of Google
+// https://bugzilla.mozilla.org/show_bug.cgi?id=689252
+user_pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+
+// PREF: When geolocation is enabled, don't log geolocation requests to the console
+user_pref("geo.wifi.logging.enabled",                           false);
+
+// PREF: Disable WebRTC entirely to prevent leaking internal IP addresses (Firefox < 42)
+// NOTICE: Disabling WebRTC breaks peer-to-peer file sharing tools (reep.io ...)
+user_pref("media.peerconnection.enabled",                       false);
+
+// PREF: Don't reveal your internal IP when WebRTC is enabled (Firefox >= 42)
+// https://wiki.mozilla.org/Media/WebRTC/Privacy
+// https://github.com/beefproject/beef/wiki/Module%3A-Get-Internal-IP-WebRTC
+user_pref("media.peerconnection.ice.default_address_only",      true); // Firefox 42-51
+user_pref("media.peerconnection.ice.no_host",                   true); // Firefox >= 52
+
+// PREF: Disable WebRTC getUserMedia, screen sharing, audio capture, video capture
+// https://wiki.mozilla.org/Media/getUserMedia
+// https://blog.mozilla.org/futurereleases/2013/01/12/capture-local-camera-and-microphone-streams-with-getusermedia-now-enabled-in-firefox/
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator
+user_pref("media.navigator.enabled",                            false);
+user_pref("media.navigator.video.enabled",                      false);
+user_pref("media.getusermedia.screensharing.enabled",           false);
+user_pref("media.getusermedia.audiocapture.enabled",            false);
+
+// PREF: Disable "beacon" asynchronous HTTP transfers (used for analytics)
+// https://developer.mozilla.org/en-US/docs/Web/API/navigator.sendBeacon
+user_pref("beacon.enabled",                                     false);
+
+// PREF: Disable speech recognition
+// https://dvcs.w3.org/hg/speech-api/raw-file/tip/speechapi.html
+// https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition
+// https://wiki.mozilla.org/HTML5_Speech_API
+user_pref("media.webspeech.recognition.enable",                 false);
+
+// PREF: Disable speech synthesis
+// https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+user_pref("media.webspeech.synth.enabled",                      false);
+
+// PREF: Disable sensor API
+// https://wiki.mozilla.org/Sensor_API
+user_pref("device.sensors.enabled",                             false);
+
+// PREF: Disable pinging URIs specified in HTML <a> ping= attributes
+// http://kb.mozillazine.org/Browser.send_pings
+user_pref("browser.send_pings",                                 false);
+
+// PREF: When browser pings are enabled, only allow pinging the same host as the origin page
+// http://kb.mozillazine.org/Browser.send_pings.require_same_host
+user_pref("browser.send_pings.require_same_host",               true);
+
+// PREF: Disable gamepad API to prevent USB device enumeration
+// https://www.w3.org/TR/gamepad/
+// https://trac.torproject.org/projects/tor/ticket/13023
+user_pref("dom.gamepad.enabled",                                false);
+
+// PREF: Disable virtual reality devices APIs
+// https://developer.mozilla.org/en-US/Firefox/Releases/36#Interfaces.2FAPIs.2FDOM
+// https://developer.mozilla.org/en-US/docs/Web/API/WebVR_API
+user_pref("dom.vr.enabled",                                     false);
+
+// PREF: Disable vibrator API
+user_pref("dom.vibrator.enabled",           false);
+
+// PREF: Disable resource timing API
+// https://www.w3.org/TR/resource-timing/#privacy-security
+user_pref("dom.enable_resource_timing", false);
+
+/******************************************************************************
+ * SECTION: Misc                                                              *
+ ******************************************************************************/
+
+// PREF: Disable GeoIP lookup on your address to set default search engine region
+// https://trac.torproject.org/projects/tor/ticket/16254
+// https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_geolocation-for-default-search-engine
+user_pref("browser.search.countryCode",                         "US");
+user_pref("browser.search.region",                              "US");
+user_pref("browser.search.geoip.url",                           "");
+
+// PREF: Set Accept-Language HTTP header to en-US regardless of Firefox localization
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
+user_pref("intl.accept_languages",                              "en-US, en");
+
+// PREF: Don't use OS values to determine locale, force using Firefox locale setting
+// http://kb.mozillazine.org/Intl.locale.matchOS
+user_pref("intl.locale.matchOS",                                false);
+
+// PREF: Don't use Mozilla-provided location-specific search engines
+user_pref("browser.search.geoSpecificDefaults",                 false);
+
+// PREF: Do not automatically send selection to clipboard on some Linux platforms
+// http://kb.mozillazine.org/Clipboard.autocopy
+user_pref("clipboard.autocopy",                                 false);
+
+/******************************************************************************
+ * SECTION: Extensions / plugins                                              *
+ ******************************************************************************/
+
+// PREF: Disable Flash Player NPAPI plugin
+// http://kb.mozillazine.org/Flash_plugin
+user_pref("plugin.state.flash",                                 0);
+
+// PREF: Disable Java NPAPI plugin
+user_pref("plugin.state.java",                                  0);
+
+// PREF: Disable sending Flash Player crash reports
+user_pref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled",     false);
+
+// PREF: When Flash crash reports are enabled,
+//       don't send the visited URL in the crash report
+user_pref("dom.ipc.plugins.reportCrashURL",                     false);
+
+// PREF: When Flash is enabled, download and use Mozilla SWF URIs blocklist
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1237198
+// https://github.com/mozilla-services/shavar-plugin-blocklist
+user_pref("browser.safebrowsing.blockedURIs.enabled",           true);
+
+// PREF: Updates addons automatically
+// https://blog.mozilla.org/addons/how-to-turn-off-add-on-updates/
+user_pref("extensions.update.enabled",                          true);
+
+// PREF: Enable add-on and certificate blocklists (OneCRL) from Mozilla
+// https://wiki.mozilla.org/Blocklisting
+// https://blocked.cdn.mozilla.net/
+// http://kb.mozillazine.org/Extensions.blocklist.enabled
+// http://kb.mozillazine.org/Extensions.blocklist.url
+// https://blog.mozilla.org/security/2015/03/03/revoking-intermediate-certificates-introducing-onecrl/
+// Updated at interval defined in extensions.blocklist.interval (default: 86400)
+user_pref("extensions.blocklist.enabled",                       true);
+user_pref("services.blocklist.update_enabled",                  true);
+
+// PREF: Decrease system information leakage to Mozilla blocklist update servers
+// https://trac.torproject.org/projects/tor/ticket/16931
+user_pref("extensions.blocklist.url", "https://blocklist.addons.mozilla.org/blocklist/3/%APP_ID%/%APP_VERSION%/");
+
+/******************************************************************************
+ * SECTION: Firefox (anti-)features / components                              *
+ ******************************************************************************/
+
+// PREF: Disable WebIDE
+// https://trac.torproject.org/projects/tor/ticket/16222
+// https://developer.mozilla.org/docs/Tools/WebIDE
+user_pref("devtools.webide.enabled",                            false);
+user_pref("devtools.webide.autoinstallADBHelper",               false);
+user_pref("devtools.webide.autoinstallFxdtAdapters",            false);
+
+// PREF: Disable remote debugging
+// https://developer.mozilla.org/en-US/docs/Tools/Remote_Debugging/Debugging_Firefox_Desktop
+// https://developer.mozilla.org/en-US/docs/Tools/Tools_Toolbox#Advanced_settings
+user_pref("devtools.debugger.remote-enabled",                   false);
+user_pref("devtools.chrome.enabled",                            false);
+user_pref("devtools.debugger.force-local",                      true);
+
+// PREF: Disable Mozilla telemetry/experiments
+// https://wiki.mozilla.org/Platform/Features/Telemetry
+// https://wiki.mozilla.org/Privacy/Reviews/Telemetry
+// https://wiki.mozilla.org/Telemetry
+// https://www.mozilla.org/en-US/legal/privacy/firefox.html#telemetry
+// https://support.mozilla.org/t5/Firefox-crashes/Mozilla-Crash-Reporter/ta-p/1715
+// https://wiki.mozilla.org/Security/Reviews/Firefox6/ReviewNotes/telemetry
+// https://gecko.readthedocs.io/en/latest/browser/experiments/experiments/manifest.html
+// https://wiki.mozilla.org/Telemetry/Experiments
+user_pref("toolkit.telemetry.enabled",                          false);
+user_pref("toolkit.telemetry.unified",                          false);
+user_pref("experiments.supported",                              false);
+user_pref("experiments.enabled",                                false);
+user_pref("experiments.manifest.uri",                           "");
+
+// PREF: Disallow Necko to do A/B testing
+// https://trac.torproject.org/projects/tor/ticket/13170
+user_pref("network.allow-experiments",                          false);
+
+// PREF: Disable sending Firefox crash reports to Mozilla servers
+// https://wiki.mozilla.org/Breakpad
+// http://kb.mozillazine.org/Breakpad
+// https://dxr.mozilla.org/mozilla-central/source/toolkit/crashreporter
+// https://bugzilla.mozilla.org/show_bug.cgi?id=411490
+// A list of submitted crash reports can be found at about:crashes
+user_pref("breakpad.reportURL",                                 "");
+
+// PREF: Disable sending reports of tab crashes to Mozilla
+//       (about:tabcrashed), don't nag user about unsent crash reports
+// https://hg.mozilla.org/mozilla-central/file/tip/browser/app/profile/firefox.js
+user_pref("browser.tabs.crashReporting.sendReport",             false);
+user_pref("browser.crashReports.unsubmittedCheck.enabled",      false);
+
+// PREF: Disable FlyWeb (discovery of LAN/proximity IoT devices
+//       that expose a Web interface)
+// https://wiki.mozilla.org/FlyWeb
+// https://wiki.mozilla.org/FlyWeb/Security_scenarios
+// https://docs.google.com/document/d/1eqLb6cGjDL9XooSYEEo7mE-zKQ-o-AuDTcEyNhfBMBM/edit
+// http://www.ghacks.net/2016/07/26/firefox-flyweb
+user_pref("dom.flyweb.enabled",                                 false);
+
+// PREF: Disable the UITour backend
+// https://trac.torproject.org/projects/tor/ticket/19047#comment:3
+user_pref("browser.uitour.enabled",                             false);
+
+// PREF: Enable Firefox Tracking Protection
+// https://wiki.mozilla.org/Security/Tracking_protection
+// https://support.mozilla.org/en-US/kb/tracking-protection-firefox
+// https://support.mozilla.org/en-US/kb/tracking-protection-pbm
+// https://kontaxis.github.io/trackingprotectionfirefox/
+// https://feeding.cloud.geek.nz/posts/how-tracking-protection-works-in-firefox/
+user_pref("privacy.trackingprotection.enabled",                 true);
+user_pref("privacy.trackingprotection.pbmode.enabled",          true);
+
+// PREF: Enable contextual identity Containers feature (Firefox >= 52)
+// NOTICE: Containers are not available in Private Browsing mode
+// https://wiki.mozilla.org/Security/Contextual_Identity_Project/Containers
+user_pref("privacy.userContext.enabled",                        true);
+
+// PREF: Enable hardening against various fingerprinting vectors (Tor Uplift project)
+// https://wiki.mozilla.org/Security/Tor_Uplift/Tracking
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1333933
+user_pref("privacy.resistFingerprinting",                       true);
+
+// PREF: Disable collection/sending of the health report (healthreport.sqlite*)
+// https://support.mozilla.org/en-US/kb/firefox-health-report-understand-your-browser-perf
+// https://gecko.readthedocs.org/en/latest/toolkit/components/telemetry/telemetry/preferences.html
+user_pref("datareporting.healthreport.uploadEnabled",           false);
+user_pref("datareporting.policy.dataSubmissionEnabled",         false);
+
+// PREF: Disable Pocket
+// https://support.mozilla.org/en-US/kb/save-web-pages-later-pocket-firefox
+// https://github.com/pyllyukko/user.js/issues/143
+user_pref("extensions.pocket.enabled",                          false);
+
+// PREF: Disable screenshot feature to the ... menu on the address bar
+user_pref("extensions.screenshots.disabled",                    true);
+
+// PREF: Disable reader mode
+user_pref("reader.parse-on-load.enabled",                       false);
+
+// PREF: Disable SHIELD
+// https://support.mozilla.org/en-US/kb/shield
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1370801
+user_pref("app.shield.optoutstudies.enabled",                   false);
+
+// PREF: Disable "Recommended by Pocket" in Firefox Quantum
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+
+/*******************************************************************************
+ * SECTION: Caching                                                            *
+ ******************************************************************************/
+
+// PREF: Disable disk cache
+// http://kb.mozillazine.org/Browser.cache.disk.enable
+user_pref("browser.cache.disk.enable",                          false);
+
+// PREF: Allow decoded images, chrome, and secure pages to be
+//       cached in memory. (Default)
+user_pref("browser.cache.memory.enable",                        true);
+
+// PREF: Longer interval between session information record (10 minutes)
+//       (Default 15 seconds)
+// https://wiki.archlinux.org/index.php/Firefox/Tweaks#Longer_interval_between_session_information_record
+user_pref("browser.sessionstore.interval",                      600000);
+
+/*******************************************************************************
+ * SECTION: UI related                                                         *
+ *******************************************************************************/
+
+// PREF: Set homepage
+user_pref("browser.startup.homepage",                           "about:home");
+
+// PREF: Always ask the user where to download
+// https://developer.mozilla.org/en/Download_Manager_preferences (obsolete)
+user_pref("browser.download.useDownloadDir",                    false);
+
+// PREF: Disable the "new tab page" feature and show a blank tab instead
+// https://wiki.mozilla.org/Privacy/Reviews/New_Tab
+// https://support.mozilla.org/en-US/kb/new-tab-page-show-hide-and-customize-top-sites#w_how-do-i-turn-the-new-tab-page-off
+user_pref("browser.newtabpage.enabled",                         false);
+
+// PREF: Disable Activity Stream
+// https://wiki.mozilla.org/Firefox/Activity_Stream
+user_pref("browser.newtabpage.activity-stream.enabled",         false);
+
+// PREF: Disable new tab tile ads & preload
+// http://www.thewindowsclub.com/disable-remove-ad-tiles-from-firefox
+// http://forums.mozillazine.org/viewtopic.php?p=13876331#p13876331
+// https://wiki.mozilla.org/Tiles/Technical_Documentation#Ping
+// https://gecko.readthedocs.org/en/latest/browser/browser/DirectoryLinksProvider.html#browser-newtabpage-directory-source
+// https://gecko.readthedocs.org/en/latest/browser/browser/DirectoryLinksProvider.html#browser-newtabpage-directory-ping
+// TODO: deprecated? not in DXR, some dead links
+user_pref("browser.newtab.preload",                             false);
+
+// PREF: Fix problem with context menu on XFCE
+//       The context menu is not rightfully positioned.
+user_pref("ui.context_menus.after_mouseup",                     true);
+
+// PREF: Disable closing the window when closing the last tab.
+user_pref("browser.tabs.closeWindowWithLastTab",                true)
+
+// PREF: Disable smooth when scrolling
+user_pref("general.smoothScroll",                               false);
