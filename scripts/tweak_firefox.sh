22e9311101a76554956021a8bd1555c563ef0040
Lzu Tao <1stpangu@gmail.com>
2018-07-29 00:51:28 +0700

    [firefox] Add installer script for firefox configuration

---
+++ scripts/tweak_firefox.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+get_firefox_profile()
+{
+  local PARENT_FF_DIR="$HOME/.mozilla/firefox"
+  local PROFILE_INI="$PARENT_FF_DIR/profiles.ini"
+
+  if [[ ! -f "$PROFILE_INI" ]]; then
+    return 1
+  fi
+
+  if grep --quiet '\[Profile[^0]\]' "$PROFILE_INI" ; then
+    PROFILE_DIR=$(\
+        grep -E '^\[Profile|^Path|^Default' "$PROFILE_INI" \
+        | grep -1 '^Default=1' \
+        | grep '^Path' | cut -d= -f2 )
+  else
+    PROFILE_DIR=$( grep 'Path=' "$PROFILE_INI" | cut -d= -f2 )
+  fi
+
+  echo "$PARENT_FF_DIR/$PROFILE_DIR"
+}
+
+start_install()
+{
+  if PROFILE_DIR=$( get_firefox_profile ); then
+    (cd "$SCRIPTPATH/.." && stow -v -t "$PROFILE_DIR" firefox)
+  fi
+}
+
+start_install
