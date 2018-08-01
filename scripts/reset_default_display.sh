47093d5c01c8682848c08da6e65ad50cb56c69b3
Lzu Tao <1stpangu@gmail.com>
2018-08-01 15:06:53 +0700

    Add script to reset display to default
    This file is useful if we disconnected second screen while pc is
    suspended, which causes main screen scale double large.

---
+++ scripts/reset_default_display.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# View setting with `xrandr -d :0`
+# or reset an out-of-range resolution with `rm ~/.config/monitors.xml`
+#     https://wiki.ubuntu.com/X/Config/Resolution
+
+xrandr -s 0
