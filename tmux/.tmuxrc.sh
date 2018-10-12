d24de0f58870f45bce13a3ca67d5baf38ef11941
Lzu Tao <1stpangu@gmail.com>
2018-10-12 09:15:00 +0700

    tmux: Better mouse support
    Signed-off-by: Lzu Tao <1stpangu@gmail.com>

--- tmux/.tmuxrc.sh
+++ tmux/.tmuxrc.sh
@@ -5,7 +5,7 @@
 # See https://repology.org/metapackage/tmux/badges
 
 # If tmux is not run, exit immediately
-[ -z "$TMUX" ] && return
+[ -z "$TMUX" ] && return 0
 
 TMUX_OS="$(uname)"
 TMUX_VERSION="$(tmux -V | cut -d' ' -f2)"
@@ -14,8 +14,8 @@ TMUX_VERSION_MINOR="$(echo "$TMUX_VERSION" | cut -d'.' -f2)"
 
 case "$TMUX_OS" in
   Darwin ) tmux source-file "${HOME}/.tmux-macos.conf" ;;
-  FreeBSD ) tmux source-file "${HOME}/.tmux-freebsd.conf" ;;
-  *) tmux display -p "Unknown OS" ;;
+  #FreeBSD ) tmux source-file "${HOME}/.tmux-freebsd.conf" ;;
+  #*) tmux display -p "Unknown OS" ;;
 esac
 
 if [ "$TMUX_VERSION_MAJOR" -le 2 ] && [ "$TMUX_VERSION_MINOR" -le 3 ]; then
