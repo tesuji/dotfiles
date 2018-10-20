adeda37be252bea6834e57493180edca30fbabb8
Lzu Tao <1stpangu@gmail.com>
2018-10-20 21:44:12 +0700

    subl: Fix deprecated use of sublime API
    This fixes that we cannot trim trailing spaces.
    
    Signed-off-by: Lzu Tao <1stpangu@gmail.com>

--- subl/.config/sublime-text-3/Packages/User/trim_trailing_white_space.py
+++ subl/.config/sublime-text-3/Packages/User/trim_trailing_white_space.py
@@ -2,30 +2,13 @@
 import sublime, sublime_plugin
 
 
-def trim_trailing_white_space(view):
-    trailing_white_space = view.find_all('[\t ]+$')
-    trailing_white_space.reverse()
-    edit = view.begin_edit()
-    for r in trailing_white_space:
-        view.erase(edit, r)
-    view.end_edit(edit)
+def trim_trailing_white_space(view, edit):
+    regions = view.find_all('[\t ]+$')
+    regions.reverse()
+    for region in regions:
+        view.erase(edit, region)
 
 
 class TrimTrailingWhiteSpaceCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        trim_trailing_white_space(self.view)
-
-
-class TrimTrailingWhiteSpace(sublime_plugin.EventListener):
-    def on_pre_save(self, view):
-        if view.settings().get('trim_trailing_white_space_on_save'):
-            trim_trailing_white_space(view)
-
-
-class EnsureNewlineAtEof(sublime_plugin.EventListener):
-    def on_pre_save(self, view):
-        if view.settings().get('ensure_newline_at_eof_on_save'):
-            if view.size() > 0 and view.substr(view.size() - 1) != '\n':
-                edit = view.begin_edit()
-                view.insert(edit, view.size(), '\n')
-                view.end_edit(edit)
+        trim_trailing_white_space(self.view, edit)
