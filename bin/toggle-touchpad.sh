1e9ef99fef1be7f310c361074962eb96d5f46388
Lzu Tao <taolzu@gmail.com>
2018-10-28 21:52:25 +0700

    bin: Use sed in place of awk for grepping info
    Signed-off-by: Lzu Tao <taolzu@gmail.com>

--- bin/toggle-touchpad.sh
+++ bin/toggle-touchpad.sh
@@ -12,15 +12,13 @@
 toggle_touchpad() {
   m_icon_enable=input-touchpad-symbolic
   m_icon_disable=touchpad-disabled-symbolic
-
   m_info_enable="Touchpad has been enabled"
   m_info_disable="Touchpad has been disabled"
 
-  # fgrep and cut is still faster than sed
-  mID="$( xinput --list | awk '$0 ~ /TouchPad/ { match($0, /id=[0-9]+/); if (RSTART) print substr($0, RSTART+3, RLENGTH-3) }' )"
-  mSTATE="$( xinput list-props "$mID" | awk '/^\tDevice Enabled \([0-9]+\):\t[01]/ { print $NF }' )"
+  mID="$( xinput --list | sed -E -n '/TouchPad/{s/^.*id=([0-9]+).*/\1/p}' )"
+  mSTATE="$( xinput list-props "$mID" | sed -E -n 's/^\tDevice Enabled \([0-9]+\):\t([01])/\1/p' )"
 
-  if [ "$mSTATE" -eq '1' ]; then
+  if [ "$mSTATE" -eq 1 ]; then
     # Should install Notification servers
     # https://wiki.archlinux.org/index.php/Desktop_notifications#Notification_servers
     xinput --disable "$mID" \
