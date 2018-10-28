54cada40b218dad310b660f8f8f556c99224c63b
Lzu Tao <taolzu@gmail.com>
2018-10-28 19:42:41 +0700

    toggle-touchpad: Use awk in place of consecutive grep and cut
    Signed-off-by: Lzu Tao <taolzu@gmail.com>

--- bin/toggle-touchpad.sh
+++ bin/toggle-touchpad.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-######################################################
+# -----------------------------------------------------------------------------
 # This script is used to enabling / disabling Touchpad
 # Use at your own risk!
-######################################################
+# -----------------------------------------------------------------------------
 
 # Ref:
+#    https://stackoverflow.com/a/18756948/5456794
 #    https://askubuntu.com/questions/844151/enable-disable-touchpad/844218
 #    https://askubuntu.com/a/537031/565006
 #    https://wiki.archlinux.org/index.php/Libinput#Disable_touchpad
@@ -16,8 +17,8 @@ toggle_touchpad() {
   m_info_disable="Touchpad has been disabled"
 
   # fgrep and cut is still faster than sed
-  mID="$( xinput | grep -F 'TouchPad' | cut -f2 | cut -d= -f2 )"
-  mSTATE="$( xinput list-props "$mID" | grep "Device Enabled" | grep -o "[01]$" )"
+  mID="$( xinput --list | awk '$0 ~ /TouchPad/ { match($0, /id=[0-9]+/); if (RSTART) print substr($0, RSTART+3, RLENGTH-3) }' )"
+  mSTATE="$( xinput list-props "$mID" | awk '/^\tDevice Enabled \([0-9]+\):\t[01]/ { print $NF }' )"
 
   if [ "$mSTATE" -eq '1' ]; then
     # Should install Notification servers
