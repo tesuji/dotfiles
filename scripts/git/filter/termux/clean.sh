a84a17fdb844adf845cd8b206bf29ee6d6fd8191
Lzu Tao <taolzu@gmail.com>
2026-09-24 05:15:47 +0700

    tidy git-filter

--- scripts/git/filter/termux/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# clean: runs when content is staged (working dir ? index).
-sed -E 's@/data/data/com.termux/files@@'
--- scripts/git/filter/xfce_user/smudge.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Restore home path in configuration file (default is /home/user)
-MY_USER=$(whoami)
-sed -E 's@(/home/)user@\1'"${MY_USER}"'@'
